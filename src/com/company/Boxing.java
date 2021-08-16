85007c44f51e22108cf304fd1cf1742f25a17b1a
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-08-16 15:59:31 -0300

    Initial commit

---
+++ src/com/company/Boxing.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+
+		Integer i2 = 127;
+		Integer i3= 127;
+
+		Integer i4 = 129;
+		Integer i5 = 129;
+
+
+		System.out.println(i2.equals(i3));
+		System.out.println(i4.equals(i5));
+
+
+	}
+}
